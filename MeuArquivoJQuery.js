f1f30b5394c532ad66f8777a2d88fa9fa890fa98
Hugo Cruz <huggorviddal64@hotmail.com>
2017-09-08 07:34:27 -0300

    Update MeuArquivoJQuery.js

--- MeuArquivoJQuery.js
+++ MeuArquivoJQuery.js
@@ -23,3 +23,8 @@ $(document).ready(function ()
         $("a").css("text-decoration", "none");
     });
 });
+
+var date = new Date();
+        var month = date.getMonth()+ 1;
+        var dateString =  month + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        console.log(dateString);
