f92ad027de1cac2940e708a2216da34ce809c4b7
Hugo Cruz <huggorviddal64@hotmail.com>
2017-09-07 21:57:07 -0300

    Cola

---
+++ MeuArquivoJQuery.js
@@ -0,0 +1,25 @@
+﻿
+function somenteNumeros(num) {
+    var er = /[^0-9.]/;
+    er.lastIndex = 0;
+    var campo = num;
+    if (er.test(campo.value)) {
+        campo.value = "";
+    }
+}
+
+function InsercaoDeZerosAEsquerda(name) {
+    var campo = name;
+    if ($(campo).val().length == 2) {
+        campo.value = "00" + campo.value;
+    }
+}
+
+$(document).ready(function ()
+{
+    $("a").click(function ()
+    {
+        $("#helloWord").css("color", "#0000FF").css("background-color", "#000000");
+        $("a").css("text-decoration", "none");
+    });
+});
